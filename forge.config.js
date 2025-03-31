46d3e59e601152bac9d7bcd74e482b9caa471216
till de roquefeuil <tillderoquefeuil@gmail.com>
2025-03-31 16:48:56 +0200

    enable macOS code signing and notarization in forge configuration

--- forge.config.js
+++ forge.config.js
@@ -11,13 +11,13 @@ module.exports = {
       '^/database.json$',
       '^/.*\.log$',
     ],
-    // osxSign: {},
-    // osxNotarize: {
-    //   tool: 'notarytool',
-    //   appleId: process.env.APPLE_ID,
-    //   appleIdPassword: process.env.APPLE_PASSWORD,
-    //   teamId: process.env.APPLE_TEAM_ID
-    // }
+    osxSign: {},
+    osxNotarize: {
+      tool: 'notarytool',
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_PASSWORD,
+      teamId: process.env.APPLE_TEAM_ID
+    }
   },
   rebuildConfig: {},
   makers: [
